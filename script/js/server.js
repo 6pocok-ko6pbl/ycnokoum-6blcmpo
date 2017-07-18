fc29ea6d878dcb5385a9e376abecd9ebd4fbf079
Real Name <i.will.never.say.my.real.name@gmail.com>
2017-07-19 03:07:33 +0500

    fix symlinks to hardlinks on source fs folder's structure

--- script/js/server.js
+++ script/js/server.js
@@ -1,1 +1,50 @@
-/mnt/sda1/folder/save/script/js/server.js
+var qs = require('querystring');
+
+var url = require('url');
+
+var http = require('http');
+ 
+var port = 8888;
+ 
+var s = http.createServer();
+
+s.on('request', function(request, response) {
+	response.writeHead(200);
+	console.log(request.method);
+	console.log(request.headers);
+	console.log(request.url);
+
+	if(request.method=='POST') {
+
+		var data = '';
+		request.on('data', function(chunk) {
+			data += chunk.toString();
+		});
+			request.on('end', function() {
+				var POST =  qs.parse(data);
+				console.log(POST);
+		});
+	}
+
+	if (data!==undefined) {
+		console.log(data);
+	}
+	else {
+		console.log('no_data_sended_by_get_request');
+	}
+
+	var url_parts = url.parse(request.url,true);
+	console.log('\r\n');
+	console.log('url_parts.query');
+	console.log('\r\n');
+	console.log(url_parts.query);
+	console.log('\r\n');
+
+	response.write('hi');
+	response.end();
+	process.exit()
+});
+ 
+s.listen(port);
+
+console.log('Browse to http://127.0.0.1:' + port);
--- script/sh/server.loop.sh
+++ script/sh/server.loop.sh
@@ -1,1 +1,10 @@
-/mnt/sda1/folder/save/script/sh/server.loop.sh
+#!/bin/sh
+
+n=0
+
+while (true)
+do
+	n=$(( n+1 ))
+	echo "Welcome $n times."
+	node "../../script/js/server.js"
+done
